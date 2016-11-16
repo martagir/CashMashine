39fd75fb48274da07be35950f4b4a22da231cd24
martagir <martagir@yandex.ru>
2016-11-16 23:40:18 +0400

    Update InterruptOperationException.java

--- exception/InterruptOperationException.java
+++ exception/InterruptOperationException.java
@@ -1,1 +1,12 @@
+package com.javarush.test.level26.lesson15.big01.exception;
+
+/**
+ * Created by ????? on 07.02.2016.
+ */
+public class InterruptOperationException extends Exception
+{
+    public InterruptOperationException()
+    {
+    }
+}
 
